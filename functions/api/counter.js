6c006f9a5d52e6182f953ecfcb508745641b072b
Brady Holt <brady.holt@gmail.com>
2022-10-27 17:53:07 -0600

    Play

--- functions/api/counter.js
+++ functions/api/counter.js
@@ -1,9 +1,18 @@
 export async function onRequest(context) {
-  let currentCounterValue = (await env.playground.get("counter")) || 0;
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
 
-  currentCounterValue++;
-  await env.playground.put("counter", currentCounterValue);
-  return new Response(currentCounterValue.toString(), {
+  let counterValue = (await env.playground.get("counter")) || 0;
+  counterValue++;
+  await env.playground.put("counter", counterValue);
+  return new Response(counterValue.toString(), {
     headers: {
       "Content-Type": "text",
     },
